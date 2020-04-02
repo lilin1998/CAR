8cc61499cc423f98c23d515ea5b32efb41aad776
eliz <e0319168@u.nus.edu>
2020-04-02 11:05:35 +0800

    update register new patient method in registrationOperationModule

--- CARSClient/src/java/carsclient/RegistrationOperationModule.java
+++ CARSClient/src/java/carsclient/RegistrationOperationModule.java
@@ -3,11 +3,17 @@ package carsclient;
 import ejb.session.stateless.AppointmentEntitySessionBeanRemote;
 import ejb.session.stateless.DoctorSessionBeanRemote;
 import ejb.session.stateless.PatientSessionBeanRemote;
+import entity.GenderEnum;
 import static entity.GenderEnum.F;
 import static entity.GenderEnum.M;
 import entity.PatientEntity;
 import entity.StaffEntity;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
 import java.util.Scanner;
+import util.exception.PasswordException;
 
 public class RegistrationOperationModule {
     private DoctorSessionBeanRemote doctorSessionBeanRemote;
@@ -30,7 +36,7 @@ public class RegistrationOperationModule {
     
     
     
-    public void registrationOperation()
+    public void registrationOperation() throws NoSuchAlgorithmException, NoSuchProviderException, PasswordException
     {
         
         Scanner scanner = new Scanner(System.in);
@@ -82,16 +88,21 @@ public class RegistrationOperationModule {
     
     
     
-    private void registerNewPatient() 
+    private void registerNewPatient() throws NoSuchAlgorithmException, NoSuchProviderException, PasswordException 
     {
         Scanner scanner = new Scanner(System.in);
         PatientEntity newPatientEntity = new PatientEntity();
         
-        System.out.println("*** CARS :: Registration Operation :: Register New Patient ***\n");
+        System.out.println("*** CARS :: Administration Operation :: Add New Patient ***\n");
         System.out.print("Enter Identity Number> ");
         newPatientEntity.setIdentityNumber(scanner.nextLine().trim());
         System.out.print("Enter Password> ");
-        newPatientEntity.setPassword(scanner.nextLine().trim());
+        
+        String passwordToHash = scanner.nextLine().trim();
+        byte[] salt = getSalt();
+        String securePassword = getSecurePassword(passwordToHash, salt);
+        newPatientEntity.setPassword(securePassword);
+        
         System.out.print("Enter First Name> ");
         newPatientEntity.setFirstName(scanner.nextLine().trim());
         System.out.print("Enter Last Name> ");
@@ -99,23 +110,70 @@ public class RegistrationOperationModule {
         System.out.print("Enter Gender> ");
         if (scanner.nextLine().trim().equals("M")) 
         {
-            newPatientEntity.setGender(M);
+            newPatientEntity.setGender(GenderEnum.M);
         } 
         else 
         {
-            newPatientEntity.setGender(F);
+            newPatientEntity.setGender(GenderEnum.F);
         }
         System.out.print("Enter Age> ");
         newPatientEntity.setAge(scanner.nextInt());
+        scanner.nextLine();
         System.out.print("Enter Phone> ");
         newPatientEntity.setPhone(scanner.nextLine().trim());
         System.out.print("Enter Address> ");
         newPatientEntity.setAddress(scanner.nextLine().trim());
 
-        Long newPatientId = patientSessionBeanRemote.createPatient(newPatientEntity);
-        System.out.println(newPatientId + "Patient has been registered successfully\n");
+        try 
+        {
+            patientSessionBeanRemote.checkPassword(passwordToHash);
+            Long newPatientId = patientSessionBeanRemote.createPatient(newPatientEntity);
+            System.out.println("Patient ID " + newPatientId + " has been added successfully\n");
+        } 
+        catch (PasswordException e) 
+        {
+            System.out.println("An error has occured while adding new patient: " + e.getMessage() + "\n");
+        }   
     }
     
+    private static String getSecurePassword(String passwordToHash, byte[] salt)
+    {
+        String generatedPassword = null;
+        try {
+            // Create MessageDigest instance for MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            //Add password bytes to digest
+            md.update(salt);
+            //Get the hash's bytes 
+            byte[] bytes = md.digest(passwordToHash.getBytes());
+            //This bytes[] has bytes in decimal format;
+            //Convert it to hexadecimal format
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
+            {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            //Get complete hashed password in hex format
+            generatedPassword = sb.toString();
+        } 
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
+     
+    //Add salt
+    private static byte[] getSalt() throws NoSuchAlgorithmException, NoSuchProviderException
+    {
+        //Always use a SecureRandom generator
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "SUN");
+        //Create array for salt
+        byte[] salt = new byte[16];
+        //Get a random salt
+        sr.nextBytes(salt);
+        //return salt
+        return salt;
+    }
     
     
     private void registerWalkinConsultation()
